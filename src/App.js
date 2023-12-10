3317d9086f43ef115d8b46d1fe773af8faa620f5
Mohnish <mohnish.a007@gmail.com>
2023-12-10 22:45:32 +0000

    framer motion added sucessfully

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
+import { motion } from "framer-motion"
 
 function App() {
   return (
     <>
-     <h1 className="text-3xl text-cyan-600 font-bold underline">
+    <div className='container items-center justify-center px-5 py-20'>
+     <motion.h1 
+      animate={{
+        scale: [1, 2, 2, 1, 1],
+        rotate: [0, 0, 270, 270, 0],
+        borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      }}
+     className="text-6xl text-center  text-cyan-600 font-bold underline">
       Hello world!
-    </h1>
+    </motion.h1>
+    </div>
     </>
   );
 }

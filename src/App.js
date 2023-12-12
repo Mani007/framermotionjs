11682817f01a33fd2b3365296bc731c18eeb960d
Mohnish <mohnish.a007@gmail.com>
2023-12-12 19:19:22 +0000

    transition delay

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { motion } from "framer-motion"
 function App() {
   return (
     <>
-    <div className='container items-center justify-center px-5 py-20'>
+    <div className=' w-full items-center justify-center px-5 py-5'>
      <motion.h1 
       animate={{
-        x: 200,
-        y: 500
+        x: 100,
+        
       }}
+      transition={{ delay: 3 }}
      className="text-6xl text-center  text-cyan-600 font-bold underline">
       Hello world!
     </motion.h1>

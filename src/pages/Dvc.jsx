a36ff1bb81d9a62a05b37534a171fd98c4d27da0
Mohnish <mohnish.a007@gmail.com>
2023-12-21 22:46:58 +0000

    center item with proper vertical line component: refer stackoverflow for arbitry value alignments

--- src/pages/Dvc.jsx
+++ src/pages/Dvc.jsx
@@ -6,12 +6,12 @@ import Dot from '../components/Dot';
 function Dvc() {
   return (
     <>
-      <div className='container '>
+      <div className='h-screen  grid content-center place-content-center items-center justify-center'>
       <div className="container  relative  mx-40">
           <Dot/>
         </div>
         <div className="container  relative  mx-40">
-          <Verticalline height='400px' width='20px' />
+          <Verticalline />
         </div>
         <br />
         <div className="container  relative  mx-20">

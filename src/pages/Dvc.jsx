7e1d14535a93bbdcf32efc3cfe40069326f5c84a
Mohnish <mohnish.a007@gmail.com>
2023-12-24 00:29:13 +0000

    span instead of div inline flex added

--- src/pages/Dvc.jsx
+++ src/pages/Dvc.jsx
@@ -9,13 +9,13 @@ function Dvc() {
     
       <div className='relative h-screen  grid  content-end pb-5 items-center justify-center'>
       <div className="container  relative  mx-[155px]">
-         <div className='inline-flex items-baseline'>
+         <span className='inline-flex items-baseline'>
           <Dot color='bg-green-500'/>
           <span className='px-5'>
           <Box text="Your Data Now" color="bg-yellow-600"/>
           </span>
           
-          </div>
+          </span>
         </div>
         <div className="container  relative  mx-40">
           <Verticalline height='h-[70px]' width='w-[5px]' rotate='rotate-0' />
